25e308312d3919f9e5994b4cfc8c262d197129f8
bsorrentino <bartolomeo.sorrentino@gmail.com>
2013-10-17 17:26:30 +0200

    site directory creation/set

--- src/main/java/org/bsc/forge/ConfluenceForgePlugin.java
+++ src/main/java/org/bsc/forge/ConfluenceForgePlugin.java
@@ -1,48 +1,136 @@
 package org.bsc.forge;
 
-import java.util.Arrays;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Inject;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.IOUtil;
 
-import org.jboss.forge.shell.ShellPrompt;
 import org.jboss.forge.shell.plugins.Alias;
-import org.jboss.forge.shell.plugins.Command;
-import org.jboss.forge.shell.plugins.DefaultCommand;
-import org.jboss.forge.shell.plugins.Option;
-import org.jboss.forge.shell.plugins.PipeIn;
 import org.jboss.forge.shell.plugins.PipeOut;
 import org.jboss.forge.shell.plugins.Plugin;
+import org.jboss.forge.shell.plugins.RequiresFacet;
+import org.jboss.forge.maven.MavenCoreFacet;
+import org.jboss.forge.project.Project;
+import org.jboss.forge.project.services.ResourceFactory;
+import org.jboss.forge.resources.DirectoryResource;
+import org.jboss.forge.shell.Shell;
+import org.jboss.forge.shell.ShellColor;
+import org.jboss.forge.shell.plugins.RequiresProject;
+import org.jboss.forge.shell.plugins.SetupCommand;
 
 /**
  *
  */
 @Alias("confluence")
+@RequiresFacet({MavenCoreFacet.class})
+@RequiresProject
 public class ConfluenceForgePlugin implements Plugin
 {
    @Inject
-   private ShellPrompt prompt;
+   private Shell/*Prompt*/ prompt;
 
-   @DefaultCommand
-   public void defaultCommand(@PipeIn String in, PipeOut out)
-   {
-      out.println("Executed default command.");
-   }
+   @Inject 
+   private Project project;
+   
+   @Inject
+   private ResourceFactory resourceFactory;
+   
+   @SetupCommand
+   public void setup(PipeOut out) {
+       
+       final MavenCoreFacet facet = project.getFacet(MavenCoreFacet.class);
+       
+       final MavenProject mProject = facet.getMavenProject();
+       
+       final String sourceDirectory = mProject.getBuild().getSourceDirectory();
+       
+       final String siteDir = String.format("%s/site/confluence", sourceDirectory);
+       
+       java.io.File siteDirFile = new java.io.File( siteDir );
+       
+       //final DirectoryResource root = project.getProjectRoot();
+       //final DirectoryResource siteDirRes = root.createFrom(siteDirFile);
+       
+       while( !siteDirFile.exists() ) {
+        
+            final boolean createFolder = prompt.promptBoolean( String.format("Do you want create missing folder [%s] ", siteDir), true);
 
-   @Command
-   public void command(@PipeIn String in, PipeOut out, @Option String... args)
-   {
-      if (args == null)
-         out.println("Executed named command without args.");
-      else
-         out.println("Executed named command with args: " + Arrays.asList(args));
-   }
+            if( createFolder ) {
 
-   @Command
-   public void prompt(@PipeIn String in, PipeOut out)
-   {
-      if (prompt.promptBoolean("Do you like writing Forge plugins?"))
-         out.println("I am happy.");
-      else
-         out.println("I am sad.");
+                final boolean success = siteDirFile.mkdirs();
+
+                if( success ) {
+                     out.println( MESG_FOLDER_CREATED);
+                     break;
+                }
+                else {
+                    out.println(ShellColor.RED, String.format("error creating folder [%s]", siteDir));
+                    return;
+                }
+            }
+            else {
+
+                final String newFolder = prompt.prompt( "Please, give me the site folder relative to ${basedir}. press enter to abort");
+
+                if( newFolder==null || newFolder.isEmpty()) {
+                     out.println( MSG_SETUP_INTERRUPTED);
+                    return;
+                }
+                siteDirFile = new java.io.File( mProject.getBasedir(), newFolder );
+
+            }
+        }
+       
+       //DirectoryResource siteDirRes =  (DirectoryResource) resourceFactory.getResourceFrom(siteDirFile);
+        
+       try {
+            final java.io.InputStream confluenceTemplatePage = getClass().getClassLoader().getResourceAsStream("template.confluence");
+            final java.io.Writer confluenceHomePage = new FileWriter( new java.io.File(siteDirFile, "home.confluence") );
+
+            IOUtil.copy( confluenceTemplatePage , confluenceHomePage);
+            
+       } catch (IOException ex) {
+           
+           out.println(ShellColor.RED, "error copying home page template ....! Set VERBOSE for details");
+                   
+           printVerbose(ex);
+           
+           Logger.getLogger(ConfluenceForgePlugin.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       try {
+            final java.io.InputStream siteTemplatePage = getClass().getClassLoader().getResourceAsStream("site.xml");
+            final java.io.Writer sitePage = new FileWriter( new java.io.File(siteDirFile, "site.xml") );
+
+            IOUtil.copy( siteTemplatePage , sitePage);
+            
+       } catch (IOException ex) {
+           
+           out.println(ShellColor.RED, "error copying site template ....! Set VERBOSE for details");
+                   
+           printVerbose(ex);
+           
+           Logger.getLogger(ConfluenceForgePlugin.class.getName()).log(Level.SEVERE, null, ex);
+       }
+        
    }
+   
+   private void printVerbose( Throwable t ) {
+   
+       java.io.StringWriter sw = new java.io.StringWriter(4096);
+       java.io.PrintWriter w = new java.io.PrintWriter( sw );
+       t.printStackTrace( w );
+       
+       prompt.printlnVerbose(ShellColor.RED, sw.toString());
+   }
+   
+    public static final String MSG_SETUP_INTERRUPTED = "setup interrupted!";
+    public static final String MESG_FOLDER_CREATED = "folder created!";
+    
+    public static final String PLUGIN_VERSION = "3.4.2";
+   
+
 }
--- src/test/java/org/bsc/forge/ConfluenceForgePluginTest.java
+++ src/test/java/org/bsc/forge/ConfluenceForgePluginTest.java
@@ -1,8 +1,16 @@
 package org.bsc.forge;
 
+import org.apache.maven.project.MavenProject;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNull;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.forge.maven.MavenCoreFacet;
+import org.jboss.forge.project.Project;
+import org.jboss.forge.project.packaging.PackagingType;
+import org.jboss.forge.resources.DirectoryResource;
 import org.jboss.forge.test.AbstractShellTest;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ConfluenceForgePluginTest extends AbstractShellTest
@@ -15,21 +23,67 @@ public class ConfluenceForgePluginTest extends AbstractShellTest
    }
 
    @Test
-   public void testDefaultCommand() throws Exception
+   public void testSetup() throws Exception
    {
-      getShell().execute("confluence");
-   }
-
-   @Test
-   public void testCommand() throws Exception
-   {
-      getShell().execute("confluence command");
-   }
-
-   @Test
-   public void testPrompt() throws Exception
-   {
+      final Project project = super.initializeProject(PackagingType.JAR);
+      Assert.assertNotNull(project);
+      Assert.assertThat( project.hasFacet( MavenCoreFacet.class), Is.is(true) );
+      
+      final MavenCoreFacet mavenFacet = project.getFacet(MavenCoreFacet.class);
+      Assert.assertThat( mavenFacet, IsNull.notNullValue() );
+      
+      final MavenProject mProject = mavenFacet.getMavenProject();
+      Assert.assertThat( mProject, IsNull.notNullValue() );
+      
+      resetInputQueue();
       queueInputLines("y");
-      getShell().execute("confluence prompt foo bar");
+      getShell().execute("confluence setup");
+
+      Assert.assertTrue(getOutput().contains(ConfluenceForgePlugin.MESG_FOLDER_CREATED));
+
+      DirectoryResource basedir = project.getProjectRoot();
+      
+      Assert.assertThat( basedir, IsNull.notNullValue() );
+      
+      final java.io.File sourceDir = new java.io.File(mProject.getBuild().getSourceDirectory());
+      Assert.assertThat( sourceDir.exists(), Is.is(true) );
+
+      final java.io.File siteDir = new java.io.File(sourceDir, "site/confluence");
+      Assert.assertThat( siteDir.exists(), Is.is(true) );
+   }
+   
+   @Test
+   public void testSetupProvideCustomFolder() throws Exception
+   {
+      final Project project = super.initializeJavaProject();
+
+      Assert.assertNotNull(project);
+      
+      resetInputQueue();
+      queueInputLines( "n", "confluenceSite", "y");
+      getShell().execute("confluence setup");
+
+      
+      Assert.assertTrue(getOutput().contains(ConfluenceForgePlugin.MESG_FOLDER_CREATED));
+      
+      DirectoryResource basedir = project.getProjectRoot();
+      
+      Assert.assertThat( basedir, IsNull.notNullValue() );
+      Assert.assertThat( basedir.getChildDirectory("confluenceSite").exists(), Is.is(true) );
+   }
+   
+   @Test
+   public void testSetupInterrupted() throws Exception
+   {
+      final Project project = super.initializeJavaProject();
+
+      Assert.assertNotNull(project);
+      
+      resetInputQueue();
+      queueInputLines( "n", "", "y");
+      getShell().execute("confluence setup");
+
+      
+      Assert.assertTrue(getOutput().contains(ConfluenceForgePlugin.MSG_SETUP_INTERRUPTED));
    }
 }
